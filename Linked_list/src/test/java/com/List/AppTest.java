83ec0b9535ce8f804d8f864a9935d65f994d36a0
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-30 23:01:34 +0530

    TEST: testing of code
    clear

--- Linked_list/src/test/java/com/List/AppTest.java
+++ Linked_list/src/test/java/com/List/AppTest.java
@@ -1,5 +1,6 @@
 package com.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,24 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void GivenValue_When30_ThenSearchTheNode()
     {
-        assertTrue( true );
+        Linked_List linkedList = new Linked_List();
+        linkedList.Add(56);
+        linkedList.Add(30);
+        linkedList.Add(70);
+        Node node = linkedList.Search(30);
+        assertEquals(30, node.data);
     }
-}
+    @Test
+    public void FindPosition_ofElement_ThenAdd40ToAfter30()
+    {
+        Linked_List list = new Linked_List();
+        int position = 3;
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+        list.InserAtParticularPosition(position, 40);
+        Node result = list.Search(40);
+        assertEquals(40, result.data);
+}}
