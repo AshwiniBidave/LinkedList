63135950fab35c90b862f7ae3ce95cce7b54a544
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-30 11:53:52 +0530

    Dispaly:Welcome Message

---
+++ LinkedList/src/main/java/com/linkedlist/SimpleLinkedList.java
@@ -0,0 +1,14 @@
+package com.linkedlist;
+
+import java.util.LinkedList;
+
+public class SimpleLinkedList {
+
+public static void main(String[] args) {
+	System.out.println("***Welcome_To_LinkedList***");
+  
+   
+
+}
+
+}
